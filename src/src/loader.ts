4618ca4b9fe1e2735efdcf96c53a10042c04d17e
Grzegorz Abramczyk <grzegorz.abramczyk@tuatara.pl>
2018-11-02 13:58:08 +0100

    Data loader

---
+++ src/src/loader.ts
@@ -0,0 +1,9 @@
+import * as fs from 'fs'
+import * as yaml from 'js-yaml'
+import * as  _ from 'lodash'
+import { setupAccounts } from './service/accounts'
+import { setupUsers } from './service/user'
+const users = _.keyBy(yaml.safeLoadAll(fs.readFileSync('data/data.yaml', 'UTF-8')), 'username' )
+setupUsers(_(users).mapValues(({username, password}) => ({username, password})).value())
+const accountsData = _(users).mapValues(({accounts}) => (accounts)).value()
+setupAccounts(accountsData)
