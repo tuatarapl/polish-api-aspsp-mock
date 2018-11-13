7087f62d498de27144c9b9f8c0ebf05be5a89783
Grzegorz Abramczyk <grzegorz.abramczyk@tuatara.pl>
2018-11-13 14:58:42 +0100

    Added web handling

--- src/src/index.ts
+++ src/src/index.ts
@@ -92,4 +92,10 @@ swagger.initializeMiddleware(polishAPISpecification, (middleware) => {
   app.use(middleware.swaggerUi())
 })
 
+app.use(express.static('web/dist'))
+app.use(express.static('static'))
+app.get('*', (req, res) => {
+    res.sendFile(`static/index.html`)
+})
+
 app.listen(port, () => trace(`Statement service mock listening on port ${port}!`))
