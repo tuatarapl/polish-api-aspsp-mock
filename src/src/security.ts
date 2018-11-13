afd8cd30c6c94582e0bb23e38fd0ad9984654270
Grzegorz Abramczyk <grzegorz.abramczyk@tuatara.pl>
2018-11-13 16:35:41 +0100

    Web security handling

---
+++ src/src/security.ts
@@ -0,0 +1,46 @@
+import * as bodyParser from 'body-parser'
+import {Router} from 'express'
+import * as session from 'express-session'
+import * as passport from 'passport'
+import {Strategy as LocalStrategy} from 'passport-local'
+import * as FileStore from 'session-file-store'
+import { logIn } from './service/user'
+export const security = Router()
+security.use(session({
+    secret: 'keyboard cat',
+    store: new (FileStore(session))({
+        secret: 'keyboard cat'
+    })
+}))
+security.use(passport.initialize())
+security.use(passport.session())
+
+passport.use(new LocalStrategy((username, password, done) => {
+    if (logIn(username, password)) {
+        done(null, {username})
+    } else {
+        done(new Error('login failed'))
+    }
+}))
+
+export const authenticate = (req, res, next) => req.user ? next() : res.status(401).send()
+
+security.get('/user', authenticate, (req, res) => res.send(req.user))
+
+security.use(bodyParser.urlencoded({
+    extended: true
+}))
+
+security.use(bodyParser.json())
+
+security.post('/login', passport.authenticate('local', {}), (req, res) => res.send(req.user))
+
+passport.serializeUser<any, string>((user, done) => {
+    done(null, user.username)
+})
+
+passport.deserializeUser<any, string>((id, done) => {
+    done(null, {
+        username: id
+    })
+})
