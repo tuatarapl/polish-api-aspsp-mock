27c1bd0523b747cec2ad7e1bf0108fb1bea5c4d6
Grzegorz Abramczyk <grzegorz.abramczyk@tuatara.pl>
2018-11-13 16:36:20 +0100

    Vue app

---
+++ src/web/src/app.ts
@@ -0,0 +1,10 @@
+import Vue from 'vue'
+import './components'
+import router from './router'
+/* eslint-disable no-unused-vars */
+const app = new Vue({
+  el: 'body #app',
+  router,
+  data: {
+  }
+})
