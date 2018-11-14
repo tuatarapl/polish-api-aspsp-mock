298348b1cda94699e2c491c2d46286778b0a12be
Grzegorz Abramczyk <grzegorz.abramczyk@tuatara.pl>
2018-11-14 09:59:24 +0100

    Consent post method

--- src/src/consent.ts
+++ src/src/consent.ts
@@ -1,7 +1,8 @@
 import * as basicAuth from 'basic-auth'
 import * as debug from 'debug'
-import {Router} from 'express'
+import {json, Router} from 'express'
 import { NextFunction, Request, Response} from 'express'
+import * as _ from 'lodash'
 import {Consent, get as getConsent, put as putConsent} from './service/consent'
 import {generateAccessCode, generateToken, lookupToken, TokenData} from './service/token'
 
@@ -21,9 +22,9 @@ const router = Router()
 export default router
 
 router.get('/confirmConsent', (req, res) => {
-    trace('/confirmConsent')
+    trace('GET /confirmConsent')
     const id = req.query.consentId
-    trace(`consentID ${id}`)
+    trace(`consentId ${id}`)
     if (id) {
         const consent = getConsent(id)
         trace(`consent ${JSON.stringify(consent)}`)
@@ -44,6 +45,71 @@ router.get('/confirmConsent', (req, res) => {
                 redirectUri.searchParams.append('code', accessCode)
                 redirectUri.searchParams.append('state', consent.state)
                 res.redirect(redirectUri.toString())
+            } else {
+                res.redirect(`/confirmation/${id}`)
+            }
+        } else {
+            res.status(500).send()
+        }
+    } else {
+        res.status(400).send()
+    }
+})
+
+function getUsername(req): string {
+    const auth = basicAuth(req)
+    trace(`auth ${auth}`)
+    if (auth) {
+        return auth.name
+    } else if (req.user) {
+        return req.user.username
+    }
+}
+
+function redirect(req: Request, res: Response, baseUrl: string, params: {[key: string]: string}) {
+    const redirectUrl = new URL(baseUrl)
+    _.forEach(params, (value, key) => redirectUrl.searchParams.append(key, value))
+    if (req.accepts('application/vnd.tuatara.redirect+json')) {
+        res.send({
+            baseUrl,
+            redirectUrl,
+            ...params
+        })
+    } else {
+        res.redirect(redirectUrl.toString())
+    }
+}
+
+router.post('/confirmConsent', json(), (req, res) => {
+    trace('POST /confirmConsent')
+    const id = req.query.consentId
+    const newConsent: Consent = req.body
+    trace(`consentId ${id}`)
+    trace(`newConsent ${JSON.stringify(newConsent)}`)
+    if (id) {
+        const oldConsent = getConsent(id)
+        trace(`consent ${JSON.stringify(oldConsent)}`)
+        if (oldConsent) {
+            const username = getUsername(req)
+            if (username) {
+                const consent: Consent = {
+                    ...oldConsent,
+                    psuId: username,
+                    scope: newConsent.scope,
+                    scope_details: newConsent.scope_details
+                }
+                putConsent(id, consent)
+                const token = generateToken({
+                    sub: username,
+                    consentId: id
+                })
+                trace(`token ${token}`)
+                const accessCode = generateAccessCode(token)
+                trace(`accessCode ${accessCode}`)
+                redirect(req, res, consent.redirectUri, {
+                    code: accessCode,
+                    state : consent.state
+                })
             } else {
                 res.redirect(`/confirmation/${id}`)
             }
