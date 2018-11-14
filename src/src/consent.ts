d0913000b2296b45696592ea73395a236271d68b
Grzegorz Abramczyk <grzegorz.abramczyk@tuatara.pl>
2018-11-14 08:38:19 +0100

    Moved consent routes to separate file

---
+++ src/src/consent.ts
@@ -0,0 +1,73 @@
+import * as basicAuth from 'basic-auth'
+import * as debug from 'debug'
+import {Router} from 'express'
+import { NextFunction, Request, Response} from 'express'
+import {Consent, get as getConsent, put as putConsent} from './service/consent'
+import {generateAccessCode, generateToken, lookupToken, TokenData} from './service/token'
+
+const trace = debug('aspsp-mock:consent')
+const router = Router()
+export default router
+
+router.get('/confirmConsent', (req, res) => {
+    trace('/confirmConsent')
+    const id = req.query.consentId
+    trace(`consentID ${id}`)
+    if (id) {
+        const consent = getConsent(id)
+        trace(`consent ${JSON.stringify(consent)}`)
+        if (consent) {
+            const auth = basicAuth(req)
+            trace(`auth ${auth}`)
+            if (auth) {
+                consent.psuId = auth.name
+                putConsent(id, consent)
+                const token = generateToken({
+                    sub: auth.name,
+                    consentId: id
+                })
+                trace(`token ${token}`)
+                const accessCode = generateAccessCode(token)
+                trace(`accessCode ${accessCode}`)
+                const redirectUri = new URL(consent.redirectUri)
+                redirectUri.searchParams.append('code', accessCode)
+                redirectUri.searchParams.append('state', consent.state)
+                res.redirect(redirectUri.toString())
+            } else {
+                res.redirect(`/confirmation/${id}`)
+            }
+        } else {
+            res.status(500).send()
+        }
+    } else {
+        res.status(400).send()
+    }
+})
+
+router.use((req: Request, res, next) => {
+    if (req.headers.authorization) {
+        const parts = req.headers.authorization.split(' ')
+        if (parts.length === 2 && parts[0] === 'Bearer') {
+            const token = parts[1]
+            const tokenData = lookupToken(token)
+            req.token = token
+            req.tokenData = tokenData
+            if (tokenData.consentId) {
+                const consentId = tokenData.consentId
+                const consent = getConsent(consentId)
+                req.consentId = consentId
+                req.consent = consent
+            }
+        }
+    }
+    next()
+})
+
+router.get('/consent/:consentId', (req, res) => {
+    const consent = getConsent(req.params.consentId)
+    if (consent) {
+        res.send(consent)
+    } else {
+        res.status(404).send()
+    }
+})
--- src/src/index.ts
+++ src/src/index.ts
@@ -8,6 +8,7 @@ import * as yaml from 'js-yaml'
 import * as path from 'path'
 import { cwd } from 'process'
 import * as swagger from 'swagger-tools'
+import consent from './consent'
 import './loader'
 import { security } from './security'
 import {Consent, get as getConsent, put as putConsent} from './service/consent'
@@ -17,42 +18,7 @@ const port = process.env.LISTEN_PORT || 3000
 const app = express()
 
 app.use(security)
-
-app.get('/confirmConsent', (req, res) => {
-    trace('/confirmConsent')
-    const id = req.query.consentId
-    trace(`consentID ${id}`)
-    if (id) {
-        const consent = getConsent(id)
-        trace(`consent ${JSON.stringify(consent)}`)
-        if (consent) {
-            const auth = basicAuth(req)
-            trace(`auth ${auth}`)
-            if (auth) {
-                consent.psuId = auth.name
-                putConsent(id, consent)
-                const token = generateToken({
-                    sub: auth.name,
-                    consentId: id
-                })
-                trace(`token ${token}`)
-                const accessCode = generateAccessCode(token)
-                trace(`accessCode ${accessCode}`)
-                const redirectUri = new URL(consent.redirectUri)
-                redirectUri.searchParams.append('code', accessCode)
-                redirectUri.searchParams.append('state', consent.state)
-                res.redirect(redirectUri.toString())
-            } else {
-                res.redirect(`/confirmation/${id}`)
-            }
-        } else {
-            res.status(500).send()
-        }
-    } else {
-        res.status(400).send()
-    }
-})
-
+app.use(consent)
 declare global {
     namespace Express {
         interface Request {
@@ -63,25 +29,6 @@ declare global {
         }
     }
 }
-
-app.use((req: Request, res, next) => {
-    if (req.headers.authorization) {
-        const parts = req.headers.authorization.split(' ')
-        if (parts.length === 2 && parts[0] === 'Bearer') {
-            const token = parts[1]
-            const tokenData = lookupToken(token)
-            req.token = token
-            req.tokenData = tokenData
-            if (tokenData.consentId) {
-                const consentId = tokenData.consentId
-                const consent = getConsent(consentId)
-                req.consentId = consentId
-                req.consent = consent
-            }
-        }
-    }
-    next()
-})
 
 const polishAPISpecification = yaml.safeLoad(fs.readFileSync('api/PolishAPI-ver2_1.yaml', 'UTF-8'))
 swagger.initializeMiddleware(polishAPISpecification, (middleware) => {
@@ -95,14 +42,7 @@ swagger.initializeMiddleware(polishAPISpecification, (middleware) => {
   app.use(middleware.swaggerRouter({useStubs: true, controllers: path.join(__dirname, 'controllers')}))
   app.use(middleware.swaggerUi())
 })
-app.get('/consent/:consentId', (req, res) => {
-    const consent = getConsent(req.params.consentId)
-    if (consent) {
-        res.send(consent)
-    } else {
-        res.status(404).send()
-    }
-})
+
 app.use(express.static('web/dist'))
 app.use(express.static('static'))
 app.get('*', (req, res) => {
