d85324815bb5de82689c102683a980af1bae7e44
Grzegorz Abramczyk <grzegorz.abramczyk@tuatara.pl>
2018-11-14 12:53:03 +0100

    Services with data for current uesr

--- src/src/data.ts
+++ src/src/data.ts
@@ -25,8 +25,26 @@ router.get('/users/:userId', (req, res) => {
     }
 })
 
+router.get('/user/', (req, res) => {
+    const user = userService.get(req.user.username)
+    if (user) {
+        res.send(user)
+    } else {
+        res.status(404).send()
+    }
+})
+
 router.get('/users/:userId/accounts', (req, res) => {
     const accounts = accountsService.list(req.params.userId)
+    if (accounts) {
+        res.send(accounts)
+    } else {
+        res.status(404).send()
+    }
+})
+
+router.get('/user/accounts', (req, res) => {
+    const accounts = accountsService.list(req.user.username)
     if (accounts) {
         res.send(accounts)
     } else {
@@ -43,6 +61,15 @@ router.get('/users/:userId/accounts/:accountNumber', (req, res) => {
     }
 })
 
+router.get('/user/accounts/:accountNumber', (req, res) => {
+    const account = accountsService.get(req.user.username, req.params.accountNumber)
+    if (account) {
+        res.send(account)
+    } else {
+        res.status(404).send()
+    }
+})
+
 router.get('/users/:userId/accounts/:accountNumber/transactions', (req, res) => {
     const transactions = transactionsService.list(req.params.userId, req.params.accountNumber)
     if (transactions) {
@@ -52,8 +79,26 @@ router.get('/users/:userId/accounts/:accountNumber/transactions', (req, res) => 
     }
 })
 
+router.get('/user/accounts/:accountNumber/transactions', (req, res) => {
+    const transactions = transactionsService.list(req.user.username, req.params.accountNumber)
+    if (transactions) {
+        res.send(transactions)
+    } else {
+        res.status(404).send()
+    }
+})
+
 router.get('/users/:userId/accounts/:accountNumber/transactions/:itemId', (req, res) => {
     const transaction = transactionsService.get(req.params.userId, req.params.accountNumber, req.params.itemId)
+    if (transaction) {
+        res.send(transaction)
+    } else {
+        res.status(404).send()
+    }
+})
+
+router.get('/user/accounts/:accountNumber/transactions/:itemId', (req, res) => {
+    const transaction = transactionsService.get(req.user.username, req.params.accountNumber, req.params.itemId)
     if (transaction) {
         res.send(transaction)
     } else {
