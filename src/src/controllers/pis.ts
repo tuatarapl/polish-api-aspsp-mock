f1e7e87dfe090d871cc42fa731e078b41a5a3ed8
Grzegorz Abramczyk <grzegorz.abramczyk@tuatara.pl>
2018-11-15 09:16:40 +0100

    Get single payment

--- src/src/controllers/pis.ts
+++ src/src/controllers/pis.ts
@@ -31,18 +31,39 @@ export const nonEEA = paymentHandler('nonEEARequest', paymentService.nonEEA)
 export const tax = paymentHandler('taxRequest', paymentService.tax)
 
 export function bundle(req: Swagger20Request<any>, res: Response) {
-    const paymentRequest = req.swagger.params.bundleRequest.value
-    trace(`paymentRequest ${JSON.stringify(paymentRequest)}`)
+    const bundleRequest = req.swagger.params.bundleRequest.value
+    trace(`bundleRequest ${JSON.stringify(bundleRequest)}`)
     trace(`tokenData ${JSON.stringify(req.tokenData)}`)
-    const addBundle = paymentService.transferBundle(req.tokenData.sub, paymentRequest)
+    const addBundle = paymentService.transferBundle(req.tokenData.sub, bundleRequest)
     trace(`addBundle ${JSON.stringify(addBundle)}`)
     const response = {
             responseHeader: {
-            requestId: paymentRequest.requestHeader.requestId,
+            requestId: bundleRequest.requestHeader.requestId,
             sendDate: moment().toISOString(),
             isCallback: false
             },
             ...addBundle
         }
     res.send(response)
+}
+
+export function getPayment(req: Swagger20Request<any>, res: Response) {
+    const payment = req.swagger.params.payment.value
+    trace(`payment ${JSON.stringify(payment)}`)
+    trace(`tokenData ${JSON.stringify(req.tokenData)}`)
+    const getPayments = paymentService.getPayments({user: req.tokenData.sub, paymentId: payment.paymentId})
+    trace(`getPayments ${JSON.stringify(getPayments)}`)
+    if (getPayments.length === 1) {
+        const response = {
+            responseHeader: {
+            requestId: payment.requestHeader.requestId,
+            sendDate: moment().toISOString(),
+            isCallback: false
+            },
+            ...getPayments[0]
+        }
+        res.send(response)
+    } else {
+        res.status(404).send()
+    }
 }
