386eab480a9c6ce45b8515846611aede5478b7ae
damian bondaruk <damian.bondaruk@tuatara.pl>
2019-06-27 13:53:44 +0200

    FIN-21 switched to https

--- src/src/index.ts
+++ src/src/index.ts
@@ -1,12 +1,15 @@
 /* istanbul ignore file */
 import * as debug from 'debug'
 import * as express from 'express'
+import * as https from 'https'
+import * as fs from 'fs'
 import consent from './consent'
 import data from './data'
 import './loader'
 import polishApi from './polish-api'
 import { security } from './security'
 import web from './web'
+
 const trace = debug('aspsp-mock')
 const port = process.env.LISTEN_PORT || 3000
 const app = express()
@@ -17,4 +20,8 @@ app.use('/data', data)
 app.use(polishApi)
 app.use(web)
 
-app.listen(port, () => trace(`Statement service mock listening on port ${port}!`))
+https.createServer({
+    key: fs.readFileSync(__dirname + '/../src/crypto/ssl/key.pem'),
+    cert: fs.readFileSync(__dirname + '/../src/crypto/ssl/cert.pem'),
+    passphrase: 'passw0rd'
+}, app).listen(port, () => trace(`Statement service mock listening on port ${port}!`))
