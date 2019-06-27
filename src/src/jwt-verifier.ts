7d39e83b1ea1908882fee71df158432f5f176365
damian bondaruk <damian.bondaruk@tuatara.pl>
2019-06-26 23:33:29 -0700

    FIN-20 cleand code

--- src/src/jwt-verifier.ts
+++ src/src/jwt-verifier.ts
@@ -9,30 +9,22 @@ export default router
 const verify = (req, res, next) => {
     const detachedSignature = req.headers['x-jws-signature']
     const tppId = req.headers['x-jws-tpp-id']
-    if (!detachedSignature) {
-        errorHandler(req, res, 401, 'INCORRECT_SIGNATURE', 'Signature is empty.')
-        return
-    } else if (!tppId) {
-        errorHandler(req, res, 401, 'TPP_ID_NOT_FOUND', 'Tpp id not found.')
-        return
-    }
-
-    const signature = detachedSignature.replace('..', `.${new Buffer(req.rawBody, 'utf8').toString('base64').split('=')[0]}.`)
-    try {
-        let fileStream = fs.readFileSync(__dirname + `/../../crypto/${tppId}.cer`)
-        if (!jws.verify(signature, 'RS256', fileStream)) {
-            errorHandler(req, res, 401, 'CERTIFICATES_NOT_SAME', 'There are various certificates.')
+    if (detachedSignature) {
+        if (tppId) {
+            const signature = detachedSignature.replace('..', `.${new Buffer(req.rawBody, 'utf8').toString('base64').split('=')[0]}.`)
+            if (isJwsSignatureVerified(req, res, tppId, signature)) {
+                next()
+            } else {
+                return
+            }
+        } else {
+            errorHandler(req, res, 401, 'TPP_ID_NOT_FOUND', 'Tpp id not found.')
             return
         }
-    } catch (err) {
-        if (err.code === 'ENOENT') {
-            errorHandler(req, res, 401, 'CERIFICATE_NOT_FOUND', 'Certificate for tpp not found.')
-        } else {
-            errorHandler(req, res, 401, 'ERROR_READING_FILE', 'Problem with reading file.')
-        }
+    } else {
+        errorHandler(req, res, 401, 'INCORRECT_SIGNATURE', 'Signature is empty.')
         return
     }
-    next()
 }
 
 function errorHandler(req, res, status: number, code: string, message: string) {
@@ -45,6 +37,24 @@ function errorHandler(req, res, status: number, code: string, message: string) {
         code: code,
         message: message
     })
+}
+
+function isJwsSignatureVerified(req, res, tppId, signature) {
+    try {
+        let fileStream = fs.readFileSync(__dirname + `/../../crypto/${tppId}.cer`)
+        if (!jws.verify(signature, 'RS256', fileStream)) {
+            errorHandler(req, res, 401, 'CERTIFICATES_NOT_SAME', 'There are various certificates.')
+            return false;
+        }
+    } catch (err) {
+        if (err.code === 'ENOENT') {
+            errorHandler(req, res, 401, 'CERIFICATE_NOT_FOUND', 'Certificate for tpp not found.')
+        } else {
+            errorHandler(req, res, 401, 'ERROR_READING_FILE', 'Problem with reading file.')
+        }
+        return false
+    }
+    return true;
 }
 
 router.use(verify)
